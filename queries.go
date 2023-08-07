756a99c53269f9111fce61b473081133c8494d8c
Adam Bright <abright@goguardian.com>
2023-08-07 12:08:09 -0400

    coerce results to strings

--- queries.go
+++ queries.go
@@ -63,7 +63,7 @@ func (q *Query) Run(ctx context.Context) error {
 		for rows.Next() {
 			// Create a slice of interface{}'s to represent each column,
 			// and a second slice to contain pointers to each item in the columns slice.
-			columns := make([]interface{}, len(cols))
+			columns := make([]string, len(cols))
 			columnPointers := make([]interface{}, len(cols))
 			for i, _ := range columns {
 				columnPointers[i] = &columns[i]
@@ -76,7 +76,7 @@ func (q *Query) Run(ctx context.Context) error {
 			// storing it in the map with the name of the column as the key.
 			m := make(map[string]interface{})
 			for i, colName := range cols {
-				val := columnPointers[i].(*interface{})
+				val := columnPointers[i].(*string)
 				m[colName] = *val
 			}
 
